43f859f3521ab1ac985558cae31b2b730504d04a
nia <daniela.sarazhinska@icloud.com>
2026-05-05 16:31:23 +0200

    prvni commit

---
+++ zaverecny_projekt_tuner/zaverecny_projekt_tuner/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace zaverecny_projekt_tuner
+{
+    internal class Program
+    {
+        /*
+         CILE:
+        - NAUCIT SE BASIC STRUKTUROVANI C SHARP STRANEK
+        - JAK BUDE PROGRAM VNIMAT VJEMY
+        - idk bro jsme lowk cooked
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
