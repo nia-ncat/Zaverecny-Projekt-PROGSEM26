6135d4921d64a949acafeeb7bdfde0f2488f0b63
nia <daniela.sarazhinska@icloud.com>
2026-05-12 16:25:35 +0200

    hodina irl - ucime se programovat stranku
    koukali jsme se na video a implemntovali v nasem filu, zatim jsme jen stihli napsat hello world na controller

---
+++ WebApp/WebApp/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace WebApp.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            string html = "<h1>Hello World!</h1>";
+            return Content(html, "text/html");
+        }
+    }
+}
